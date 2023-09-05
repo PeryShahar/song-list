b414ab84ebadaf6d16402c65244c36198764db2a
PeryShahar <pery1shahar@gmail.com>
2023-09-05 17:16:13 +0300

    fix: remove unnecessary from docker compose & update csv service

--- packages/server/src/csv/csv.service.ts
+++ packages/server/src/csv/csv.service.ts
@@ -14,31 +14,29 @@ export class CsvService {
       ){}
 
     async insertCsvFileToDb() {
-        console.log('hi')
         try {
             const csvFilePath = '../assets/song_list.csv';
 
-            // const stream = fs.createReadStream(csvFilePath);
-            // console.log('stream: ', stream);
+            const stream = fs.createReadStream(csvFilePath);
       
-            // stream.pipe(csvParser()) // Pipe the CSV stream through csv-parser
-            //   .on('data', async (row) => {
-            //     const songName = row['Song Name'].trim().toLowerCase();
-            //     const band = row['Band'].trim().toLowerCase();
-            //     const year = row['Year'].trim().toLowerCase();
+            stream.pipe(csvParser()) // Pipe the CSV stream through csv-parser
+              .on('data', async (row) => {
+                const songName = row['Song Name'].trim().toLowerCase();
+                const band = row['Band'].trim().toLowerCase();
+                const year = row['Year'].trim().toLowerCase();
       
-            //     // Create a new Song entity
-            //     const song = new Song();
-            //     song.songName = songName;
-            //     song.band = band;
-            //     song.year = year;
+                // Create a new Song entity
+                const song = new Song();
+                song.songName = songName;
+                song.band = band;
+                song.year = year;
       
-            //     // Insert the song data into the database using the SongsService
-            //     await this.songsService.insertSong(song);
-            //   })
-            //   .on('end', () => {
-            //     console.log('CSV file processing finished.');
-            //   });
+                // Insert the song data into the database using the SongsService
+                await this.songsService.insertSong(song);
+              })
+              .on('end', () => {
+                console.log('CSV file processing finished.');
+              });
           } catch (error) {
             throw new Error(`Error inserting CSV data into the database: ${error.message}`);
           }
