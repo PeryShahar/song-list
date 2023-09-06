212c9cb5242c327800dc1222ade9b6f89f80a05c
PeryShahar <pery1shahar@gmail.com>
2023-09-06 09:43:08 +0300

    refactor: remove imports

--- packages/server/src/songs/songs.controller.ts
+++ packages/server/src/songs/songs.controller.ts
@@ -1,9 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { SongsService } from './songs.service';
-import { CreateSongDto } from './dto/create-song.dto';
-import { UpdateSongDto } from './dto/update-song.dto';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
-import { SongDto } from './dto/song.dto';
+import {  ApiTags } from '@nestjs/swagger';
 
 @ApiTags('songs')
 @Controller('songs')
