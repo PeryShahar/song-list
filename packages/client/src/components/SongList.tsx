13a36417e28cf43a90ae7463d30cfccab83bff45
PeryShahar <pery1shahar@gmail.com>
2023-09-06 17:34:21 +0300

    fix: update try/catch to fetch & update albums to songs

--- packages/client/src/components/SongList.tsx
+++ packages/client/src/components/SongList.tsx
@@ -1,44 +1,50 @@
 import { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 interface Song {
-    id: number;
-    songName: string;
-    band: string;
-    year: number;
+  id: number;
+  songName: string;
+  band: string;
+  year: number;
 }
 
 export default function SongList() {
-    const [songs, setSongs] = useState<Song[]>([])
+  const [songs, setSongs] = useState<Song[]>([]);
 
-    useEffect(() => {
-        fetchAlbums()
-    }, [])
+  useEffect(() => {
+    fetchSongs();
+  }, []);
 
-    const fetchAlbums = async () => {
-        const { data } = await axios.get<Song[]>("http://localhost:3001/songs")
-        setSongs(data)
+  const fetchSongs = async () => {
+    try {
+      const { data } = await axios.get<Song[]>("http://localhost:3001/songs");
+      setSongs(data);
+    } catch (error) {
+      console.error("Error fetching songs:", error);
     }
+  };
 
-    return <main>
-        <h1 className="song-list-title">Song List</h1>
-        <table className="song-list-table">
-            <thead>
-                <tr>
-                    <th>Song name</th>
-                    <th>Band</th>
-                    <th>Year</th>
-                </tr>
-            </thead>
-            <tbody>
-                {songs?.map((song) => (
-                    <tr key={song.id}>
-                        <td>{song.songName}</td>
-                        <td>{song.band}</td>
-                        <td>{song.year}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+  return (
+    <main>
+      <h1 className="song-list-title">Song List</h1>
+      <table className="song-list-table">
+        <thead>
+          <tr>
+            <th>Song name</th>
+            <th>Band</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          {songs?.map((song) => (
+            <tr key={song.id}>
+              <td>{song.songName}</td>
+              <td>{song.band}</td>
+              <td>{song.year}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </main>
+  );
 }
